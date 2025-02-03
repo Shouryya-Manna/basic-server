1ed827c819f018b88209dbbeadd920880cb4b01a
Shouryya <shouryyamanna06@gmail.com>
2025-02-03 17:00:28 +0530

    ommited the favicon render log

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
+  if(req.url==="/favicon.ico") return res.end();
   const log = `${Date.now()}:${req.url} New Request Received\n`;
   fs.appendFile("log.txt", log, (err, data) => {
+    
     switch(req.url){
         case "/":
             res.end("HomePage");
