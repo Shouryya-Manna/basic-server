f9e21bdedfa3a21722c53154b78f45ebf1bf926d
Shouryya <shouryyamanna06@gmail.com>
2025-02-05 01:52:08 +0530

    search query implemented in log files

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const fs = require("fs");
 const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-  if(req.url==="/favicon.ico") return res.end();
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}:${req.url} New Request Received\n`;
-  const myUrl = url.parse(req.url,true);
+  const myUrl = url.parse(req.url, true);
   console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
-    
-    switch(myUrl.pathname){
-        case "/":
-            res.end("HomePage");
-            break;
-        case "/about":
-            const username = myUrl.query.myname 
-            res.end(`Hi ${username}`);
-            break;
-        case "/search":
-          const search = myUrl.query.search
-          res.end(`Here are your results for ${search}`);
-        default:
-            res.end("Error 404");
+    switch (myUrl.pathname) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        const username = myUrl.query.myname;
+        res.end(`Hi ${username}`);
+        break;
+      case "/search":
+        const search = myUrl.query.search_query;
+        res.end(`Here are your results for ${search}`);
+        break;
+      default:
+        res.end("Error 404");
     }
   });
-  console.log(req.headers);
+  //console.log(req.headers);
 });
 myServer.listen(8000, () => console.log("Server has stared!"));
